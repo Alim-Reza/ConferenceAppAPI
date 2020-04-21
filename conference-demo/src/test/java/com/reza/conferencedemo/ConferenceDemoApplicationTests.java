300961549d07badd825d42e5b91a56a31785820d
alim-ahmed-reza <alim@cokreates.com>
2020-04-21 23:33:23 +0600

    project skeleton

---
+++ conference-demo/src/test/java/com/reza/conferencedemo/ConferenceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reza.conferencedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
