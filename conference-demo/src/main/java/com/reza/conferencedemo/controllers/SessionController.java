2125461859ce8eb8d966950c50a25d7362dfcfec
alim-ahmed-reza <alim@cokreates.com>
2020-04-25 14:55:31 +0600

    added create, update and delete controller

--- conference-demo/src/main/java/com/reza/conferencedemo/controllers/SessionController.java
+++ conference-demo/src/main/java/com/reza/conferencedemo/controllers/SessionController.java
@@ -2,11 +2,9 @@ package com.reza.conferencedemo.controllers;
 
 import com.reza.conferencedemo.models.Session;
 import com.reza.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +23,22 @@ public class SessionController {
     @RequestMapping("{id}")
     public Session get(@PathVariable Long id){
         return sessionRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Session create(@RequestBody final Session session){
+        return sessionRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value="{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        sessionRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id, @RequestBody Session session){
+        Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
     }
 }
